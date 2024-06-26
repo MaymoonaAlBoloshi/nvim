8dfe77f3128d8ffa3bc346c9a983355fcf64e10e
MaymoonaAlBoloshi <maymoona.balushi@rihal.om>
2024-06-26 10:20:37 +0400

    added ulitsnip with some base typescriptreact.snippets

---
+++ lua/plugins/ultisnip.lua
@@ -0,0 +1,8 @@
+return {
+    "SirVer/ultisnips",
+    config = function()
+        vim.g.UltiSnipsExpandTrigger = "<C-l>"
+        vim.g.UltiSnipsJumpForwardTrigger = "<C-;>"
+        vim.g.UltiSnipsJumpBackwardTrigger = "<C-'>"
+    end,
+}
