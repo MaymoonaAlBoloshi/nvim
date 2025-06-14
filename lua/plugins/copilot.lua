a93ea5a4c9ce28478d4bc816914f05e939987ec1
MaymoonaAlBoloshi <maymoona.balushi@rihal.om>
2025-06-14 21:55:56 +0400

    re enable copilot auto complete on tab, disabled by mistake

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
 	"github/copilot.vim",
 	config = function()
-		vim.g.copilot_no_tab_map = true -- Optional: Avoid Copilot mapping Tab automatically.
+		vim.g.copilot_no_tab_map = false -- Optional: Avoid Copilot mapping Tab automatically.
 	end,
 }
